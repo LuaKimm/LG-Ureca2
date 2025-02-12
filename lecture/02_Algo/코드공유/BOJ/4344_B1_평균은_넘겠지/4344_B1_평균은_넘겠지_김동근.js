13405cc61a42209e5d98bfc43ff2d9a3ff6b58fe
student <student@M402INS>
2025-02-12 19:59:39 +0900

    평균은 넘겠지 - 코드 수정

--- lecture/02_Algo/코드공유/BOJ/4344_B1_평균은_넘겠지/4344_B1_평균은_넘겠지_김동근.js
+++ lecture/02_Algo/코드공유/BOJ/4344_B1_평균은_넘겠지/4344_B1_평균은_넘겠지_김동근.js
@@ -1,2 +1,26 @@
-let number = new Array(5);
-console.log(number)
+const fs = require('fs');
+const filePath = process.platform === 'linux'?"/dev/stdin":__dirname+"/input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const inputCount = Number(input[0]);
+const testCases = [];
+
+for (let i = 1; i <= inputCount; i++) {
+  const temp = input[i].split(" ").map(Number);
+  const testCase = {
+    N: temp[0],
+    scores: temp.slice(1),
+  };
+
+  testCases.push(testCase);
+}
+
+//console.log(testCases);
+
+const solution = ({N, scores})=>{
+  const average = scores.reduce((acc, cur) => acc+cur,0)/N;
+  const count =scores.reduce((count, score)=>(score>average?count+1:count),0)
+  console.log(`${((count/N)*100).toFixed(3)}%`);
+}
+
+testCases.forEach((testCase) => solution(testCase));
