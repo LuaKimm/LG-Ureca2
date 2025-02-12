697db7175f8ea80e967b5c17b00e80f205bfdfef
BaeJia <qowldk1030@gmail.com>
2025-02-12 11:46:42 +0900

    add: 17478번 JAVA 풀이입니다.

--- lecture/02_Algo/코드공유/BOJ/17478_S5_재귀_함수가_뭔가요/17478_S5_재귀_함수가_뭔가요_배지아.java
+++ lecture/02_Algo/코드공유/BOJ/17478_S5_재귀_함수가_뭔가요/17478_S5_재귀_함수가_뭔가요_배지아.java
@@ -6,11 +6,6 @@ import java.io.InputStreamReader;
 
 public class baek17478 {
 	static int n;
-	static String question = "\"재귀함수가 뭔가요?\"";
-	static String answer1 = "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.";
-		
-	static String answer2 = "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.";
-	static String answer3 = "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"";
 
 	public static void main(String[] args) throws IOException {
 		
